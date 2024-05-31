c8b799d226141c919775927642a2b542dfb650f3
Maksim Smeljov <mshmeljov@gmail.com>
2024-05-31 16:14:12 +0300

    Homework-4 NOT READY!

--- src/Body/CoinInfo/CoinInfo.jsx
+++ src/Body/CoinInfo/CoinInfo.jsx
@@ -1,8 +1,20 @@
+import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Chart from "./Chart";
+import { getMoreData } from "../../api/assets";
 
-function CoinInfo({ coinData }) {
+function CoinInfo(coinData) {
+    const [detailedCoinData, setDetailedCoinData] = React.useState({});
+
+    React.useEffect(() => {
+        getMoreData(coinData.id).then((json) => setDetailedCoinData(json)); 
+      }, [coinData.id]);
+
+
+
+
+
     return (
         <>
         <Row>
@@ -13,12 +25,12 @@ function CoinInfo({ coinData }) {
         </Row>
         </Col>
         <Col>
-            <div>High 70 000</div>
-            <div>Low 67 000</div>
+            <div>{coinData.maxSupply}</div>
+            <div>{coinData.priceUsd}</div>
         </Col>
         <Col>
-            <div>Average 69 000</div>
-            <div>Change -3%</div>
+            <div>{coinData.vwap24Hr}</div>
+            <div>{coinData.changePercent24Hr}</div>
         </Col>
         </Row>
         <Row>
@@ -30,3 +42,40 @@ function CoinInfo({ coinData }) {
 }
 
 export default CoinInfo;
+
+
+
+///
+
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+// import Chart from "./Chart";
+
+// function CoinInfo({ coinData }) {
+//     return (
+//         <>
+//         <Row>
+//         <Col>
+//         <Row>
+//             <Col>Logo</Col>
+//             <Col>{coinData.name}</Col>
+//         </Row>
+//         </Col>
+//         <Col>
+//             <div>High 70 000</div>
+//             <div>Low 67 000</div>
+//         </Col>
+//         <Col>
+//             <div>Average 69 000</div>
+//             <div>Change -3%</div>
+//         </Col>
+//         </Row>
+//         <Row>
+//             <Chart coinData={coinData} />
+//         </Row>
+  
+//     </>
+//     );
+// }
+
+// export default CoinInfo;
--- src/api/assets.js
+++ src/api/assets.js
@@ -11,3 +11,11 @@ export const getAssetsHistory = async (id, interval) => {
 
     return await response.json();
 };
+
+
+
+export const getMoreData = async (id) => {
+  const response = await fetch(`${apiUrl}/assets/${id}`);
+
+  return await response.json();
+};
