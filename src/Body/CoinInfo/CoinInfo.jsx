252033b29622e0b42c79a3ac7f25bc77a3e21f76
Maksim Smeljov <mshmeljov@gmail.com>
2024-05-31 17:37:23 +0300

    get assets by ID

--- src/Body/CoinInfo/CoinInfo.jsx
+++ src/Body/CoinInfo/CoinInfo.jsx
@@ -1,32 +1,42 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Chart from "./Chart";
+import { getAssetsById } from "../../api/assets";
+import React from "react";
 
 function CoinInfo({ coinData }) {
-    return (
-        <>
-        <Row>
+  const [coinInfo, setCoinInfo] = React.useState({});
+
+  React.useEffect(() => {
+    getAssetsById(coinData.id).then((json) => setCoinInfo(json.data));
+  }, [coinData.id]);
+
+  return (
+    <>
+      <Row>
         <Col>
-        <Row>
+        <div>Rank: {coinInfo.rank}</div>
+        </Col>
+        <Col>
+          <Row>
             <Col>Logo</Col>
             <Col>{coinData.name}</Col>
-        </Row>
+          </Row>
         </Col>
         <Col>
-            <div>High 70 000</div>
-            <div>Low 67 000</div>
+          <div>High 70 000</div>
+          <div>Low 67 000</div>
         </Col>
         <Col>
-            <div>Average 69 000</div>
-            <div>Change -3%</div>
+          <div>Average {coinInfo.vwap24Hr}</div>
+          <div>Change {coinInfo.changePercentage24Hr}%</div>
         </Col>
-        </Row>
-        <Row>
-            <Chart coinData={coinData} />
-        </Row>
-  
+      </Row>
+      <Row>
+        <Chart coinData={coinData} />
+      </Row>
     </>
-    );
+  );
 }
 
 export default CoinInfo;
