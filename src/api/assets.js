804e182feae6c5e5152882f64410d3400cf53dba
Maksim Smeljov <mshmeljov@gmail.com>
2024-05-31 19:41:45 +0300

    moment, downgrade, recharts, period

--- src/api/assets.js
+++ src/api/assets.js
@@ -6,9 +6,14 @@ export const getAssets = async () => {
   return await response.json();
 };
 
-export const getAssetsHistory = async (id, interval) => {
+export const getAssetsHistory = async (id, interval, start, end) => {
+  const params = new URLSearchParams({
+     interval,
+     start,
+     end,
+  });
   const response = await fetch(
-    `${apiUrl}/assets/${id}/history?interval=${interval}`
+    `${apiUrl}/assets/${id}/history?${params}`
   );
 
   return await response.json();
